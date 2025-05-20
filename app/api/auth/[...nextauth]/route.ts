909a437475417d8b1e1cdcd7763f6a71b2c49f8f
phev8 <phev8@users.noreply.github.com>
2025-05-20 08:39:48 +0200

    Add logging for detected host and protocol in auth routing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import { handlers } from "@/auth";
+import logger from "@/lib/logger";
 import { NextRequest } from "next/server";
 
 const basePath = process.env.BASEPATH ?? '';
@@ -11,6 +12,8 @@ function rewriteRequest(req: NextRequest) {
 
     const detectedHost = headers.get("x-forwarded-host") ?? host;
     const detectedProtocol = headers.get("x-forwarded-proto") ?? protocol;
+    logger.info(`detectedHost: ${detectedHost}`);
+    logger.info(`detectedProtocol: ${detectedProtocol}`);
     const _protocol = `${detectedProtocol.replace(/:$/, "")}:`;
     const url = new URL(_protocol + "//" + detectedHost + basePath + pathname + search);
 
