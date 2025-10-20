d93b365d02a4994dc989baaffb4d58ca9d0c327b
phev8 <phev8@users.noreply.github.com>
2025-10-20 12:16:15 +0200

    use auto-login

--- app/(public)/login/_components/client-login.tsx
+++ app/(public)/login/_components/client-login.tsx
@@ -3,10 +3,20 @@
 import { login } from "@/actions/login"
 import LoadingButton from "@/components/loading-button"
 import { LogIn } from "lucide-react"
-import { useTransition } from "react"
+import { useSearchParams } from "next/navigation"
+import { useEffect, useTransition } from "react"
 
 const ClientLogin = () => {
     const [isPending, startTransition] = useTransition()
+    const searchParams = useSearchParams()
+
+    const autoLogin = searchParams.get('autoLogin') === 'true'
+
+    useEffect(() => {
+        if (autoLogin) {
+            performLogin()
+        }
+    }, [autoLogin])
 
     const performLogin = () => {
         startTransition(async () => {
